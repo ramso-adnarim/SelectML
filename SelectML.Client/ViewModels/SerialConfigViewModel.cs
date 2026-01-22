81ed0655c574d7bbf07c6a5c9ce086ff595b9586
Mitutoyo "BSG" Brasil <measurlinkbr@gmail.com>
2026-01-21 23:49:07 -0300

    Ajuste de diretiva para Client.Services no SerialConfigViewModel.cs

--- SelectML.Client/ViewModels/SerialConfigViewModel.cs
+++ SelectML.Client/ViewModels/SerialConfigViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Media;
+using SelectML.Client.Services;
 
 namespace SelectML.Client.ViewModels
 {
